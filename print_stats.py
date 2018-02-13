ba1d40cee8e3c87602e8d71ca8a347dad5a58565
Okhlopkov Daniil Olegovich <okhlopkov@phystech.edu>
2018-02-13 23:46:08 +0300

    adopt to new package

--- print_stats.py
+++ print_stats.py
@@ -1,13 +1,18 @@
+import os
 import sys
 import json
 
-import stats
+import statstools.stats as stats
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         keys_path = "./keys.json"
     else:
         keys_path = sys.argv[1]
+
+    if not os.path.isfile(keys_path):
+        print("Can't find `keys.json` file. See `keys_template.json`.")
+        exit()
 
     with open(keys_path, 'r') as fin:
         keys = json.load(fin)
@@ -21,6 +26,7 @@ if __name__ == '__main__':
             change_info = stats.get_portfolio_info(exchange)
             stats.print_portfolio_info(change_info)
         except Exception as e:
-            print('Failed to get data for {}. Reason: {}'.format(key['Description'], e))
+            print('Failed to get data for {}. Reason: {}'.format(
+                key['Description'], e))
             continue
         print()
